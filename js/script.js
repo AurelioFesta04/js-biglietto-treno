a71874909eb8fff4fd51c0ccb902cabd9633a025
AurelioFesta04 <auryfesta@gmail.com>
2024-10-28 17:23:53 +0100

    refactoring: bonus finished

--- js/script.js
+++ js/script.js
@@ -17,4 +17,14 @@ if (userEta < 18) {
     ticketPriceOff = (ticketPrice - (ticketPrice * 0.40))
 }
 
+// output
 console.log(ticketPriceOff.toFixed(2))
+
+// bonus
+if (isNaN(userKm)) {
+    alert("inserisci dei valori numerici nel campo dei km !!!")
+}
+
+if (isNaN(userEta)) {
+    alert("inserisci dei valori numerici nel campo dell'età !!!")
+}
